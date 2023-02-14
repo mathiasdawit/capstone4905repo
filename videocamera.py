526fdca02313370cdad390e9e87e590799868a90
LiamK206 <122187260+LiamK206@users.noreply.github.com>
2023-02-14 13:56:28 -0500

    Update videocamera.py

--- videocamera.py
+++ videocamera.py
@@ -3,7 +3,11 @@ import numpy as np
 import math
 from enum import Enum
 
-# from .grip_update import GripPipeline
+import self as self
+from playsound import playsound
+from grip import GripPipeline
+
+pipeline = GripPipeline()
 
 vid = cv2.VideoCapture(0)
 
@@ -17,7 +21,7 @@ frame_width = int(vid.get(5))
 frame_height = int(vid.get(6))
 
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'A'), 10, (frame_width, frame_height))
+#out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'A'), 10, (frame_width, frame_height))
 
 while (True):
 
@@ -25,14 +29,34 @@ while (True):
     # by frame
     ret, frame = vid.read()
     vidgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
     if ret == True:
 
         ret, thresh = cv2.threshold(vidgray, 127, 255, 0)
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(vidgray, contours, -1, (0, 255, 0), 3)
+
+        contour_data = []
+
+        try:
+            # Process the Grip Pipeline
+            pipeline.process(frame)
+
+            # Populate data from contours
+            contour_data = pipeline.find_contours_output
+
+        except (ZeroDivisionError):
+            self.logger.logMessage("Divide by 0 exception in GRIP Pipeline")
+
+
+        cv2.drawContours(frame, contour_data, -1, (0, 255, 0), 3)
+
+
+
 
         # Display the resulting frame
-        cv2.imshow('frame', vidgray)
+        cv2.imshow('frame', frame)
+
 
         # the 'q' button is set as the
         # quitting button
@@ -43,8 +67,11 @@ while (True):
 
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+#while (True):
+#    playsound('beep-01a')
+
 # After the loop release the cap object
 vid.release()
-out.release()
+#out.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
